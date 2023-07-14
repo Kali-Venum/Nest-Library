7f976ec1324d06dac48769c8980f0609da5581fe
weaverswebsubham <subham.das@weavers-web.com>
2023-07-14 18:11:48 +0530

    working on the jwt configuration.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,22 @@ import { AppService } from './app.service';
 import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
 import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+
+// Schema.
+import { UserSchema } from './auth/schemas/user.schema';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    // JwtModule.registerAsync({
+    //   inject: 
+    // }),
+    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
+  ],
   controllers: [AppController, AuthController],
   providers: [AppService, AuthService],
 })

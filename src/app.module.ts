827df14b29bd73bf994e343cd912155e705dfc8e
weaverswebsubham <subham.das@weavers-web.com>
2023-07-14 18:32:03 +0530

    mongoose & .env configured.

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
 
 // Schema.
 import { UserSchema } from './auth/schemas/user.schema';
@@ -14,10 +15,15 @@ import { BookModule } from './book/book.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.DB_URI),
     AuthModule,
     PassportModule.register({ defaultStrategy: 'jwt' }),
     // JwtModule.registerAsync({
-    //   inject: 
+    //   inject:ConfigModule
     // }),
     MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
     BookModule,

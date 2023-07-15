bf9fda1e1f146055b5958f0cffeffb3d34a4d33c
Kali-Venum <subhamdas.abcd@gmail.com>
2023-07-15 11:05:43 +0530

    working on the register service.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+
+// Schema.
+import { User } from './schemas/user.schema';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectModel(User.name)
+    private UserModel: Model<User>,
+    private jwtService: JwtService,
+  ) {}
+
+  async register(SignUpDTO) {
+    const { name, email, password } = SignUpDTO;
+
+    const hashedPassword = bcrypt.hash(password, 10);
+
+    const user = await this.UserModel.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    const token = this.jwtService.sign({ id: user._id });
+
+    return {token}
+  }
+}
